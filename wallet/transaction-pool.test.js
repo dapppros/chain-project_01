6d5b6c984dee15fbb2225ea73a093373d9eee9e5
zblockdoc <zblockdoc@gmail.com>
2023-11-02 13:08:56 -0400

    Transaction Broadcast

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -1,17 +1,36 @@
-class TransactionPool {
-  constructor() {
-    this.transactionMap = {};
-  }
+const TransactionPool = require('./transaction-pool');
+const Transaction = require('./transaction');
+const Wallet = require('./index');
 
-  setTransaction(transaction) {
-    this.transactionMap[transaction.id] = transaction;
-  }
+describe('TransactionPool', () => {
+  let transactionPool, transaction, senderWallet;
 
-  existingTransaction({ inputAddress }) {
-    const transactions = Object.values(this.transactionMap);
+  beforeEach(() => {
+    transactionPool = new TransactionPool();
+    senderWallet = new Wallet();
+    transaction = new Transaction({
+      senderWallet,
+      recipient: 'fake-recipient',
+      amount: 50
+    });
+  });
 
-    return transactions.find(transaction => transaction.input.address === inputAddress);
-  }
-}
+  describe('setTransaction()', () => {
+    it('adds a transaction', () => {
+      transactionPool.setTransaction(transaction);
 
-module.exports = TransactionPool;
+      expect(transactionPool.transactionMap[transaction.id])
+        .toBe(transaction);
+    });
+  });
+
+  describe('existingTransaction()', () => {
+    it('returns an existing transaction given an input address', () => {
+      transactionPool.setTransaction(transaction);
+
+      expect(
+        transactionPool.existingTransaction({ inputAddress: senderWallet.publicKey })
+      ).toBe(transaction);
+    });
+  });
+});
