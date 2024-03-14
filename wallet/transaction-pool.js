c9ed1d6d85cb0a25a9d064bf27a93af26c9972d5
zblockdoc <iabouzeid@zblockdoc.com>
2024-03-14 03:45:12 -0400

    API Transactions and Main Transaction pool

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -1,11 +1,5 @@
-const Transaction = require('./transaction');
-
 class TransactionPool {
   constructor() {
-    this.transactionMap = {};
-  }
-
-  clear() {
     this.transactionMap = {};
   }
 
@@ -13,32 +7,10 @@ class TransactionPool {
     this.transactionMap[transaction.id] = transaction;
   }
 
-  setMap(transactionMap) {
-    this.transactionMap = transactionMap;
-  }
-
   existingTransaction({ inputAddress }) {
     const transactions = Object.values(this.transactionMap);
 
     return transactions.find(transaction => transaction.input.address === inputAddress);
-  }
-
-  validTransactions() {
-    return Object.values(this.transactionMap).filter(
-      transaction => Transaction.validTransaction(transaction)
-    );
-  }
-
-  clearBlockchainTransactions({ chain }) {
-    for (let i=1; i<chain.length; i++) {
-      const block = chain[i];
-
-      for (let transaction of block.data) {
-        if (this.transactionMap[transaction.id]) {
-          delete this.transactionMap[transaction.id];
-        }
-      }
-    }
   }
 }
 
