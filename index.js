fcbb73340644324c0f3eb745187e46ab251d9a67
zblockdoc <iabouzeid@zblockdoc.com>
2024-03-18 23:26:54 -0400

    Sync Transaction pool

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
-const pubsub = new PubSub({ blockchain });
+const pubsub = new PubSub({ blockchain, transactionPool });
 
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
@@ -34,13 +34,28 @@ app.post('/api/mine', (req, res) => {
 app.post('/api/transact', (req, res) => {
   const { amount, recipient } = req.body;
 
-  const transaction = wallet.createTransaction({ recipient, amount });
+  let transaction = transactionPool
+    .existingTransaction({ inputAddress: wallet.publicKey });
+
+  try {
+    if (transaction) {
+      transaction.update({ senderWallet: wallet, recipient, amount });
+    } else {
+      transaction = wallet.createTransaction({ recipient, amount });
+    }
+  } catch(error) {
+    return res.status(400).json({ type: 'error', message: error.message });
+  }
 
   transactionPool.setTransaction(transaction);
 
-  console.log('transactionPool', transactionPool);
+  pubsub.broadcastTransaction(transaction);
 
-  res.json({ transaction });
+  res.json({ type: 'success', transaction });
+});
+
+app.get('/api/transaction-pool-map', (req, res) => {
+  res.json(transactionPool.transactionMap);
 });
 
 const syncChains = () => {
